7508a92df5db42aaea68bedb38e4472a72abd461
SyedFouzan <syedfouzanofficial@gmail>
2026-04-10 16:47:56 +0530

    Deployed to the App

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("Node App is running successfully!");
+	res.send("Node App is running successfully and it is hosted on the PORT!");
 });
 
 const PORT = 3000;
